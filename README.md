4001bcd449fc09bbedc050a878677d6d40ad87b7
Brandon Valley <bavvh8@mst.edu>
2022-12-07 08:27:35 -0600

    sandbox test works

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tik_live_host
+
+https://tikapi.io/documentation/#tag/Live/operation/user.live.chat
+
+pip3 install tikapi
+
+here for api key when done with sandbox server:  https://tikapi.io/documentation/#section/Getting-Started/Installation
+
+## Sandbox:
+
+Enabling sandbox server in Python:
+
+api.set(
+    __sandbox__=True
+)
+
+Sandbox API Key:
+
+DemoAPIKeyTokenSeHYGXDfd4SFD320Sc39Asd0Sc39Asd4s
+
+Sandbox Account Key:
+
+DemoAccountKeyTokenSeHYGXDfd4SFD320Sc39Asd0Sc39A
--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
-print('init')
+from pprint import pprint
+
+
+from tikapi import TikAPI, ValidationException, ResponseException
+
+api = TikAPI("DemoAPIKeyTokenSeHYGXDfd4SFD320Sc39Asd0Sc39Asd4s")
+
+api.set(
+    __sandbox__=True
+)
+
+try:
+    response = api.public.check(
+        username="lilyachty"
+    )
+
+    pprint(response.json())
+
+except ValidationException as e:
+   pprint(e, e.field)
+
+except ResponseException as e:
+    pprint(e, e.response.status_code)
+
+# DemoAPIKeyTokenSeHYGXDfd4SFD320Sc39Asd0Sc39Asd4s
+# 
+# Sandbox Account Key:
+# 
+# DemoAccountKeyTokenSeHYGXDfd4SFD320Sc39Asd0Sc39A
