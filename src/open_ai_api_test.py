6f0b5eab4ed357768e04bd65cf3c961ff2deb8d5
Brandon Valley <bavvh8@mst.edu>
2022-12-08 08:11:39 -0600

    got good fish joke

--- src/open_ai_api_test.py
+++ src/open_ai_api_test.py
@@ -7,6 +7,45 @@ import openai
 openai.organization = "org-0iQE6DR7AuGXyEw1kD4poyIg"
 # openai.api_key = os.getenv(SECRETS.open_ai_api_key)
 openai.api_key = SECRETS.open_ai_api_key
-print(openai.Model.list())
+# print(openai.Model.list())
+
+print("starting test")
+
+params = {
+  "model": "text-davinci-003",
+  "prompt": "Say this is a test",
+  "max_tokens": 7,
+  "temperature": 0,
+  "top_p": 1,
+  "n": 1,
+  "stream": False,
+#   "logprobs": null,
+  "logprobs": None,
+  "stop": "\n"
+}
+
+# print(openai.Model(id, api_key, api_version, api_type, organization, response_ms, api_base, engine))
+# print(openai.Completion.)
+
+
+# list engines
+engines = openai.Engine.list()
+
+# print the first engine's id
+print(engines.data[0].id)
+
+# create a completion
+# completion = openai.Completion.create(engine="ada", prompt="Hello world")
+completion = openai.Completion.create(
+#     engine="ada", 
+    model = "text-davinci-003",
+    prompt="Tell me a joke about a fish.",
+    temperature = 1,
+    max_tokens=30
+    )
+
+# print the completion
+print(completion.choices[0].text)
+
 
 print('Done')
