f03fff180301363ba0c561000b2a58dbcdb896ac
Brandon Valley <bavvh8@mst.edu>
2022-12-08 08:54:07 -0600

    get_roast_str_from_username()

--- src/open_ai_api_test.py
+++ src/open_ai_api_test.py
@@ -11,18 +11,31 @@ openai.api_key = SECRETS.open_ai_api_key
 
 print("starting test")
 
-params = {
-  "model": "text-davinci-003",
-  "prompt": "Say this is a test",
-  "max_tokens": 7,
-  "temperature": 0,
-  "top_p": 1,
-  "n": 1,
-  "stream": False,
-#   "logprobs": null,
-  "logprobs": None,
-  "stop": "\n"
-}
+def get_roast_str_from_username(username):
+    completion = openai.Completion.create(
+    model = "text-davinci-003",
+#     prompt="Tell me a joke about a fish.",
+#     prompt='Roast-bot, I command you to roast user: "MeatballMama55"',
+    prompt='Roast-bot, roast this user based on their username: "{' + username + '}"',
+#     temperature = 2,
+    max_tokens=30
+    )
+    
+    return completion.choices[0].text
+#     print(completion.choices)
+
+# params = {
+#   "model": "text-davinci-003",
+#   "prompt": "Say this is a test",
+#   "max_tokens": 7,
+#   "temperature": 0,
+#   "top_p": 1,
+#   "n": 1,
+#   "stream": False,
+# #   "logprobs": null,
+#   "logprobs": None,
+#   "stop": "\n"
+# }
 
 # print(openai.Model(id, api_key, api_version, api_type, organization, response_ms, api_base, engine))
 # print(openai.Completion.)
@@ -37,15 +50,19 @@ print(engines.data[0].id)
 # create a completion
 # completion = openai.Completion.create(engine="ada", prompt="Hello world")
 completion = openai.Completion.create(
-#     engine="ada", 
     model = "text-davinci-003",
-    prompt="Tell me a joke about a fish.",
-    temperature = 1,
+#     prompt="Tell me a joke about a fish.",
+#     prompt='Roast-bot, I command you to roast user: "MeatballMama55"',
+    prompt='Roast-bot, roast this user based on their username: "MeatballMama55"',
+#     temperature = 2,
     max_tokens=30
     )
 
 # print the completion
-print(completion.choices[0].text)
+
+
+roast_str = get_roast_str_from_username("MeatballMama55")
+print(roast_str)
 
 
 print('Done')
