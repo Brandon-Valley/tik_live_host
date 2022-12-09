5faa5a851e3cb270b5c4cad975ceacd52f2e16bb
Brandon <bavvh8@mst.edu>
2022-12-09 14:28:58 -0600

    logging works

--- src/full_live_test.py
+++ src/full_live_test.py
@@ -5,8 +5,15 @@ from tikapi import TikAPI, ValidationException, ResponseException
 from pprint import pprint
 
 
+from sms.logger import json_logger
+
+import os
+
+SCRIPT_PARENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+LOG_JSON_FILE_PATH = os.path.join(SCRIPT_PARENT_DIR_PATH, "logs", "v1_0_live_test_logs.json")
+
 def main():
-    print("Starting Main")
+    ("Starting Main")
     
     User = tik_api_handler.get_User()
     
@@ -14,6 +21,11 @@ def main():
         response = User.live.chat(
             room_id="7112492061034646278"
         )
+        
+        resp_l = []
+        
+        resp_l.append(response.json())
+        json_logger.write(resp_l, LOG_JSON_FILE_PATH)
     
         pprint(response.json())
     
@@ -23,6 +35,10 @@ def main():
             print("Getting next items ", nextCursor)
             response = response.next_items()
             pprint(response.json())
+            
+            resp_l.append(response.json())
+            json_logger.write(resp_l, LOG_JSON_FILE_PATH)
+    
     
     except ValidationException as e:
         print(e, e.field)
